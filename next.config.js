9be083de4569be1bab851676e5b01f3518942296
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2024-01-10 02:20:19 +0300

    Add an updated Next-pwa version.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
-
 const nextConfig = {};
 
 module.exports = withPWA(nextConfig);
