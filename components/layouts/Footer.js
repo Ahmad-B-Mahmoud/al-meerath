f297c697a971c33398eef3b360bf95b88d8351e8
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2024-04-23 00:01:09 +0300

    Adjust Footer.

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -1,11 +1,12 @@
 import * as React from "react";
+import useStore from "@/store";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { IconButton, Tooltip } from "@mui/material";
 import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
-import BugReportRoundedIcon from "@mui/icons-material/BugReportRounded";
+import Contact from "../forms/contact/Contact";
 
 function Copyright() {
   return (
@@ -17,42 +18,50 @@ function Copyright() {
 }
 
 export default function Footer() {
+  // Variables:
+  const openContactForm = useStore((state) => state.openContactForm);
+
+  // Handlers:
+  const handleContact = () => {
+    openContactForm();
+  };
+
   return (
-    <Box
-      component="footer"
-      sx={{
-        pt: 3,
-        px: 2,
-        mt: "auto",
-      }}
-    >
-      <Container maxWidth="md" className="glass-background">
-        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Box>
-            <Typography variant="body1" sx={{ pt: 2 }}>
-              تم تطوير تطبيق الميراث بعوّن اللّه و توفيقه.
-            </Typography>
-            <Copyright />
+    <>
+      <Contact />
+      <Box
+        component="footer"
+        sx={{
+          pt: 3,
+          px: 2,
+          mt: "auto",
+        }}
+      >
+        <Container maxWidth="sm" className="glass-background">
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box>
+              <Typography variant="body1" sx={{ pt: 2 }}>
+                تم تطوير تطبيق الميراث بعوّن اللّه و توفيقه.
+              </Typography>
+              <Copyright />
+            </Box>
+            <Box
+              sx={{ py: 2, display: "flex", justifyContent: "space-evenly" }}
+            >
+              <Tooltip title="فيس بوك">
+                <IconButton aria-label="Facebook Link">
+                  <FacebookTwoToneIcon color="primary" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="تواصل معنا">
+                <IconButton onClick={handleContact} aria-label="E-mail Contact">
+                  <EmailRoundedIcon color="primary" />
+                </IconButton>
+              </Tooltip>
+            </Box>
           </Box>
-          <Box sx={{ py: 2, display: "flex", justifyContent: "space-evenly" }}>
-            <Tooltip title="فيس بوك">
-              <IconButton aria-label="Facebook Link">
-                <FacebookTwoToneIcon color="primary" />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="تواصل معنا">
-              <IconButton aria-label="E-mail Contact">
-                <EmailRoundedIcon color="primary" />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="تبليغ عن خطأ بحلّ المسائل">
-              <IconButton aria-label="ٌReport an issue">
-                <BugReportRoundedIcon color="primary" />
-              </IconButton>
-            </Tooltip>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+    </>
   );
 }
